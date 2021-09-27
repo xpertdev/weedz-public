b58fbc7e14543fdc7fef097b28e00391abeefcd9
xpertdev <xpertdev@users.noreply.github.com>
2021-09-27 22:34:27 +0000

    deploy: xpertdev/Weedz@103eb0bb18fd031788728748716a9966bc49e43b

---
+++ _nuxt/static/1632782065/dispensary/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/dispensary", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1632782065/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdispensary","\u002Finspire","\u002Fdoctor","\u002Fstore","\u002Fusa\u002Fdoctor","\u002Fusa\u002Fdispensary"]})
