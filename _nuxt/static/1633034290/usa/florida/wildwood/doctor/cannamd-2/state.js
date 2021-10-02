d205e56ef70fbe770501f3aaf476f7e57536ca62
xpertdev <xpertdev@users.noreply.github.com>
2021-10-02 19:53:57 +0000

    deploy: xpertdev/Weedz@db19a68dd4467a33f3e8330c95d6c9a7ad8f5a19

--- _nuxt/static/1633034290/usa/florida/wildwood/doctor/cannamd-2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633034290",layout:"default",error:a,state:{filters:{country:"usa",state:a,city:a,listingType:"all"}},serverRendered:true,routePath:"\u002Fusa\u002Fflorida\u002Fwildwood\u002Fdoctor\u002Fcannamd-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1633034290/usa/ohio/newark/dispensary/verdant-creations-newark/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633034290",layout:"default",error:a,state:{filters:{country:"usa",state:a,city:a,listingType:"all"}},serverRendered:true,routePath:"\u002Fusa\u002Fohio\u002Fnewark\u002Fdispensary\u002Fverdant-creations-newark",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
